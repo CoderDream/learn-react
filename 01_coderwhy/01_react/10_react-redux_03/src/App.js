04d4d9e2d1028763751d45f6910f323460c34dfa
CoderDream <coderdream@gmail.com>
2024-01-08 18:18:55 +0800

    版本3
    完成home2.js和about2.js的重构

---
+++ 01_coderwhy/01_react/10_react-redux_03/src/App.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+import Home from './pages/home2';
+import About from './pages/about2';
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Home/>
+        <About/>
+      </div>
+    )
+  }
+}
