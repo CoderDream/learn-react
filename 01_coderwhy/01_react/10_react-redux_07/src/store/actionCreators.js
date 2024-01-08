18ecfe556cbc1be59cf3f799580b2a90e1be1134
CoderDream <coderdream@gmail.com>
2024-01-08 21:01:54 +0800

    版本7，结合网络请求

---
+++ 01_coderwhy/01_react/10_react-redux_07/src/store/actionCreators.js
@@ -0,0 +1,52 @@
+import {
+  ADD_NUMBER,
+  SUB_NUMBER,
+  INCREMENT,
+  DECREMENT,
+  CHANGE_BANNERS,
+  CHANGE_RECOMMENDS
+} from './constants.js';
+
+// export function addAction(num) {
+//   return {
+//     type: "ADD_NUMBER",
+//     num
+//   }
+// }
+
+// export const addAction = (num) => {
+//   return {
+//     type: "ADD_NUMBER",
+//     num
+//   }
+// }
+
+export const addAction = num => ({
+  type: ADD_NUMBER,
+  num
+});
+
+export const subAction = num => ({
+  type: SUB_NUMBER,
+  num
+});
+
+export const incAction = () => ({
+  type: INCREMENT
+});
+
+export const decAction = () => ({
+  type: DECREMENT
+});
+
+// 轮播图和推荐的action
+export const changeBannersAction = (banners) => ({
+  type: CHANGE_BANNERS,
+  banners
+});
+
+export const changeRecommendsAction = (recommends) => ({
+  type: CHANGE_RECOMMENDS,
+  recommends
+});
+
